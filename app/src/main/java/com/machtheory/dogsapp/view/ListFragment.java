c5cd3d26e7dda79d486c144b2c16719a8211297c
MachTheory <dwhitehead90@yahoo.com>
2020-07-17 22:40:01 -0500

    Initial commit

---
+++ app/src/main/java/com/machtheory/dogsapp/view/ListFragment.java
@@ -0,0 +1,26 @@
+package com.machtheory.dogsapp.view;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.machtheory.dogsapp.R;
+
+public class ListFragment extends Fragment {
+
+    public ListFragment() {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_list, container, false);
+    }
+}
